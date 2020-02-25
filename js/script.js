351317b88c0e2755f76dacc0a0087d3743658b2e
Saeed Panahi <S.Panahai@mobtakeran.local>
2020-02-25 14:03:47 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+
+  checkHeaderStatus()
+  window.scroll(function() {
+    checkHeaderStatus()
+  })
+
+  function checkHeaderStatus() {
+    $(window).on('activate.bs.scrollspy', function () {
+      let hash = $('.main-nav').find('a.active').attr('href');
+      if (hash !== '#top-header') {
+        $('header nav').addClass('inbody');
+      } else {
+        $('header nav').removeClass('inbody');
+      }
+    })
+  }
+})
