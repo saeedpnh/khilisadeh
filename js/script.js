d1cf35bb5f4adabaa6cedc2b807d2eba894f606f
Saeed Panahi <saeedpnh@gmail.com>
2020-04-09 14:31:16 +0430

    first parth of UI finished

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function () {
 
   checkHeaderStatus()
-  window.scroll(function() {
+  window.scroll(function () {
     checkHeaderStatus()
   })
 
@@ -15,4 +15,31 @@ $(function () {
       }
     })
   }
+
+  // wow reveal when the user scrolls
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  })
+  wow.init();
+
+
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(document).on("click", "a.js-scroll-trigger", function (e) {
+    var anchor = $(this);
+    $("html,body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor.attr("href")).offset().top - 50
+        },
+        1200,
+        "easeInOutCirc"
+      );
+    e.preventDefault();
+  });
+  
 })
